fbe24e91a50eeec65788c335d51df2328fa9c56c
Alexandre Resende <alexandre.amres@gmail.com>
2017-01-16 21:46:02 -0200

    New randomquotemachine with apis

---
+++ newRandomQuoteMachine/script/app.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+
+	$('#newQuote').on('click', function(){
+		console.log('New Quote');
+	});
+
+	$('#tweetIt').on('click', function(){
+		console.log('Tweet it.');
+	});
+
+});

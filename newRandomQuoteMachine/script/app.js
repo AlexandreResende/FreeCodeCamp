6fb5ab7ee722636db1ba3467562b6243d43610af
Alexandre Resende <alexandre.amres@gmail.com>
2017-01-18 22:59:33 -0200

    new random quote machine done and submitted to FCC

--- newRandomQuoteMachine/script/app.js
+++ newRandomQuoteMachine/script/app.js
@@ -2,11 +2,45 @@
 $(document).ready(function(){
 
 	$('#newQuote').on('click', function(){
-		console.log('New Quote.');
+		
+		function getQuote(){
+
+			$.ajax({
+					url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+					type: 'POST',
+					headers: {
+						"X-Mashape-Key": "gcPuVOpglDmshtzOwHTp2VUlC2C6p1WKbexjsnS4eFKiOPMu6p",
+						"Content-Type": "application/x-www-form-urlencoded",
+						"Accept": "application/json"
+					}
+				})
+				.done(function(data){
+					data = JSON.parse(data);
+					$('#quote').text(data.quote);
+					$('#author').text(data.author);
+				})
+				.fail(function( xhr, status, error){
+					console.log('Sorry, there was a problem!');
+					console.log('Status: ' + status);
+					console.log('Error: ' + error);
+				})
+				.always(function(){
+					console.log('AJAX request to the server was successful.');
+				});
+
+		};
+
+		getQuote();
 	});
 
 	$('#tweetIt').on('click', function(){
-		console.log('Tweet it.');
+		
+		var tweet = $('#quote').text();
+		var author = $('#author').text();
+
+		$(this).attr('href', 'https://twitter.com/intent/tweet?text='+tweet+' Author:'+author);
+
 	});
 
 });
+

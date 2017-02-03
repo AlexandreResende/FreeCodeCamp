c1a47c05782accfc60f5e2d2af2068350c5d30c9
Alexandre Resende <alexandre.amres@gmail.com>
2017-02-02 22:25:05 -0200

    Finished the project - may need some refactorization

--- wikipediaViewer/script/app.js
+++ wikipediaViewer/script/app.js
@@ -1,15 +1,13 @@
 
 $(document).ready(function(){
 
-	jQuery.support.cors = true;
+	//jQuery.support.cors = true;
 
 	$('#search').on('click', function(){
 
 		var textToBeSearched = $('#inputBox').val();
 		var urlSearched = 'https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=' + 
 						   encodeURIComponent(textToBeSearched);
-
-		console.log(textToBeSearched);
 
 		$.ajax({
 			url: urlSearched,
@@ -18,8 +16,36 @@ $(document).ready(function(){
 			crossDomain: true
 		})
 		.done(function(data) {
-			console.log("success");
-			console.log(data);
+
+			if (data.query){
+
+				var pages         = data.query.pages;
+				var wikiUrl       = 'https://en.wikipedia.org/wiki/';
+				var openElement   = '<a class="btn btn-block btn-info elementsFound" href="';
+				var middleElement = '" target="_blank"><div>';
+				var closeElement  = '</div></a>'; 
+
+				$('.closeSearch').css('display', 'block');
+				$('.searchElements').css('margin-top', '5%');
+
+				for (var page in pages){
+
+					var titleOfCurrentPage = pages[page].title;
+
+					$('.father').append(openElement + 
+										wikiUrl +
+										titleOfCurrentPage.replace(/[\s]+/g, '_') +
+										middleElement +
+										titleOfCurrentPage +
+										closeElement);
+
+				}
+
+			}
+			else {
+				alert('Searched element not found.');
+			}
+			
 		})
 		.fail(function() {
 			console.log("error");
@@ -32,11 +58,14 @@ $(document).ready(function(){
 
 	$('.closeSearch').on('click', function(){
 
-		
+		$('.closeSearch').css('display', 'none');
+		$('.searchElements').css('margin-top', '20%');
+		$('.elementsFound').remove();
+		$('#inputBox').val('');
 		
 	});
 
 });
 
-//para um artigo randomico usar a seguinte url: https://en.wikipedia.org/wiki/Special:Random
-//settar a url da pagina para a acima que irá automaticamente para um artigo randomico
+
+

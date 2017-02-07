fbb6c3f8f6fa2908b0a5b409098016e5bff5a534
Alexandre Resende <alexandre.amres@gmail.com>
2017-02-06 23:09:46 -0200

    Changed index.html and created the file with API connection

--- twitchTvApi/script/app.js
+++ twitchTvApi/script/app.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 
+	var twitchChannels = ['freecodecamp', 'loxodontes', 'quin69', 'riotgamesbrazil', 'brunofin '];	
 	
 	
+
 });
---
+++ twitchTvApi/script/twitchAPIConnection.js
@@ -0,0 +1,11 @@
+
+/*
+**This function will make the twitch api connection and
+**use jquery to modify the index.html with the specific
+**channel
+*/
+function twitchAPIConnection(channelName, elementId){
+
+
+
+}

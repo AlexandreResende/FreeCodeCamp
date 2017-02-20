dc71756f81e7e32c140329039bda75ecc162090e
Alexandre Resende <alexandre.amres@gmail.com>
2017-02-19 22:31:39 -0300

    object oriented project - major changes

--- twitchTvApi/script/app.js
+++ twitchTvApi/script/app.js
@@ -1,16 +1,32 @@
 
 $(document).ready(function(){
 
-	var twitchChannels = ['freecodecamp'];//, 'loxodontes', 'quin69', 'riotgamesbrazil', 'brunofin ', 'picoca'];	
-	
-	twitchChannels.forEach( (channel, index) => {
-		console.log(channel);
-		twitchAPIConnection(channel, index);
-
-	});
+	twitchApplication.connectAllChannels();
 
 	$('.channelContainer').on('click', function(){
 		$(this).toggleClass('active');
+	});
+
+	$('.addChannel').on('click', function() {
+		
+		var channelName = $('.channelName').value();
+
+		twitchApplication.addChannel(channelName);
+
+	});
+
+	$('removeChannel').on('click', function(){
+
+		//to be tested and implemented
+		var removeArray = $('.active');
+		var removeChannels = [];
+
+		removeArray.forEach( element => {
+			removeChannels.push(element.value());
+		});
+
+		twitchApplication.removeChannels(removeChannels);
+
 	});
 
 });
--- twitchTvApi/script/twitchAPIConnection.js
+++ twitchTvApi/script/twitchAPIConnection.js
@@ -1,9 +1,66 @@
 
+var twitchApplication = {
+
+	twitchChannels: ['freecodecamp'],//, 'loxodontes', 'quin69', 'riotgamesbrazil', 'brunofin ', 'picoca'];
+
+	connection: function(channel){
+		var url = 'https://api.twitch.tv/kraken/streams/' + channel;
+
+		$.ajax({
+			url: url,
+			headers: {
+				'Client-ID': 'z6qd7dtxvbs8oz6yik78d9yci86xsf'
+			},
+			type: 'GET',
+			dataType: 'json',
+		})
+		.done(function(data) {
+
+			//var element = '#channel' + (channelArrayPosition+1);
+
+			//$(element).parent().attr('href', 'https://www.twitch.tv/' + channel);
+
+			console.log(data);
+
+		})
+		.fail(function(xhr, error) {
+			console.log('An error occured: ' + error);
+		})
+		.always(function() {
+			console.log("Something happened...");
+		});
+	},
+
+	connectAllChannels: function(){
+
+		this.twitchChannels.forEach( (channel, index) => {
+			this.connection(channel);
+		});
+	},
+
+	addChannel: function(channelName){
+		this.twitchChannels.push(channelName);
+		console.log(this.twitchChannels);
+	},
+
+	removeChannel: function(channelNameArray, channelsArray){
+		channelNameArray.forEach( element => {
+			var position = channelsArray.indexOf(element);
+			channelsArray.splice(position, 1);
+		});
+		//currently working
+		console.log(this.twitchChannels);
+	}
+
+};
+
+
+
 /*
 **This function will make the twitch api connection and
 **use jquery to modify the index.html with the specific
 **channel
-*/
+
 function twitchAPIConnection(channelName, channelArrayPosition){
 
 	var url = 'https://api.twitch.tv/kraken/streams/' + channelName;
@@ -32,5 +89,5 @@ function twitchAPIConnection(channelName, channelArrayPosition){
 		console.log("Something happened...");
 	});
 	
-
 }
+*/

ab3addaaa80f8b62448b43273cde619bf159b986
Alexandre Resende <alexandre.amres@gmail.com>
2017-02-24 22:43:54 -0300

    added online channel functionality

--- twitchTvApi/script/twitchAPIConnection.js
+++ twitchTvApi/script/twitchAPIConnection.js
@@ -1,54 +1,53 @@
-
 var twitchApplication = {
 
-	twitchChannels: ['arteezy', 'loxodontes'],//, 'loxodontes', 'quin69', 'riotgamesbrazil', 'brunofin ', 'picoca'];
+    twitchChannels: ['summit1g'], //, 'loxodontes', 'quin69', 'riotgamesbrazil', 'brunofin ', 'picoca'];
 
-	connection: function(channel, index){
-		var url = 'https://api.twitch.tv/kraken/streams/' + channel;
+    connection: function(channel, index) {
+        var url = 'https://api.twitch.tv/kraken/streams/' + channel;
 
-		$.ajax({
-			url: url,
-			headers: {
-				'Client-ID': 'z6qd7dtxvbs8oz6yik78d9yci86xsf'
-			},
-			type: 'GET',
-			dataType: 'json',
-		})
-		.done(function(data) {
+        $.ajax({
+                url: url,
+                headers: {
+                    'Client-ID': 'z6qd7dtxvbs8oz6yik78d9yci86xsf'
+                },
+                type: 'GET',
+                dataType: 'json',
+            })
+            .done(function(data) {
 
-			twitchAPIView.showChannels(channel, index, data);
+                twitchAPIView.showChannels(channel, index, data);
 
-			console.log(data);
+                console.log(data);
 
-		})
-		.fail(function(xhr, error) {
-			console.log('An error occured: ' + error);
-		})
-		.always(function() {
-			console.log("Something happened...");
-		});
-	},
+            })
+            .fail(function(xhr, error) {
+                console.log('An error occured: ' + error);
+            })
+            .always(function() {
+                console.log("Something happened...");
+            });
+    },
 
-	connectAllChannels: function(){
+    connectAllChannels: function() {
 
-		this.twitchChannels.forEach( (channel, index) => {
-			this.connection(channel, index);
-		});
-	},
+        this.twitchChannels.forEach((channel, index) => {
+            this.connection(channel, index);
+        });
+    },
 
-	addChannel: function(channelName){
-		this.twitchChannels.push(channelName);
-		console.log(this.twitchChannels);
-	},
+    addChannel: function(channelName) {
+        this.twitchChannels.push(channelName);
+        console.log(this.twitchChannels);
+    },
 
-	removeChannel: function(channelNameArray, channelsArray){
-		channelNameArray.forEach( element => {
-			var position = channelsArray.indexOf(element);
-			channelsArray.splice(position, 1);
-		});
-		//currently working
-		console.log(this.twitchChannels);
-	}
+    removeChannel: function(channelNameArray, channelsArray) {
+        channelNameArray.forEach(element => {
+            var position = channelsArray.indexOf(element);
+            channelsArray.splice(position, 1);
+        });
+        //currently working
+        console.log(this.twitchChannels);
+    }
 
 };
 
--- twitchTvApi/script/twitchAPIView.js
+++ twitchTvApi/script/twitchAPIView.js
@@ -1,54 +1,66 @@
-
 var twitchAPIView = {
 
-	showChannels: function(channel, index, data){
+    showChannels: function(channel, index, data) {
 
-		var containerOfChannels = [];
+        var containerOfChannels = [];
 
-		//url of the channel
-		var url = 'https://www.twitch.tv/' + channel;
-		var div, divImg, img, divInfo, anchor;
+        //url of the channel
+        var url = 'https://www.twitch.tv/' + channel;
+        var div, divImg, img, divInfo, anchor, game, status;
 
-		/*
-		**strem on   => object
-		**stream off => null
-		*/
-		if (data.stream != null){
+        /*
+         **strem on   => object
+         **stream off => null
+         */
+        if (data.stream != null) {
 
-			/* example of a channel container
-			<div id="channel1" class="channelContainer">
-				<div class="channelImg">
-					<img  src="https://static-cdn.jtvnw.net/jtv_user_pictures/arteezy-profile_image-f2f55f87a5cd6e15-300x300.png">
-				</div>	
-				<div class="channelInfo">
-					<a class="btn btn-block" href="https://www.twitch.tv/arteezy" target="_blank">Arteezy</a>	
-				</div>					
-			</div>
-			*/
+            /* example of a channel container
+            <div id="channel1" class="channelContainer">
+            	<div class="channelImg">
+            		<img  src="https://static-cdn.jtvnw.net/jtv_user_pictures/arteezy-profile_image-f2f55f87a5cd6e15-300x300.png">
+            	</div>	
+            	<div class="channelInfo">
+            		<a class="btn btn-block" href="https://www.twitch.tv/arteezy" target="_blank">Arteezy</a>	
+            	</div>					
+            </div>
+            */
 
-		}else{
+            div = $('<div id="channel' + (index + 1) + '" class="channelContainer"></div>');
+            divImg = $('<div class="channelImg"></div>');
+            img = $('<img src="' + data.stream.channel.logo + '" alt="' + channel + '" >');
+            divInfo = $('<div class="channelInfo text-center"></div>');
+            anchor = $('<a class="text-primary" href="' + url + '" target="_blank">' + channel + '</a>');
+            game = $('<p>' + data.stream.channel.game + '</p>');
+            status = $('<p> Status: ' + data.stream.channel.status + '</p>');
 
-			div     = $('<div id="channel' + (index+1) + '" class="channelContainer"></div>');
-			divImg  = $('<div class="channelImg"></div>');
-			img     = $('<span class="offlineChannel">Offline</span>');
-			divInfo = $('<div class="channelInfo text-center"></div>');
-			anchor  = $('<a class="text-primary" href="' + url +'" target="_blank">' + channel + '</a>');
+            divImg.append(img);
+            divInfo.append(anchor).append(game).append(status);
+            div.append(divImg).append(divInfo);
 
-			
-			divImg.append(img);
-			divInfo.append(anchor);
-			div.append(divImg).append(divInfo);
 
-		}
+        } else {
 
-		var height = $('.twitch').height();
+            div = $('<div id="channel' + (index + 1) + '" class="channelContainer"></div>');
+            divImg = $('<div class="channelImg"></div>');
+            img = $('<span class="offlineChannel">Offline</span>');
+            divInfo = $('<div class="channelInfo text-center"></div>');
+            anchor = $('<a class="text-primary" href="' + url + '" target="_blank">' + channel + '</a>');
 
-		if (index){
-			$('.twitch').height( height + 150);
-		}
 
-		$('.channelsHolder').append(div);
+            divImg.append(img);
+            divInfo.append(anchor);
+            div.append(divImg).append(divInfo);
 
-	}
+        }
+
+        var height = $('.twitch').height();
+
+        if (index) {
+            $('.twitch').height(height + 150);
+        }
+
+        $('.channelsHolder').append(div);
+
+    }
 
 }
